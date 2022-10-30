2af5c13dd9f44bf06a742e4419b2c2c613b1b875
Tucker J. Pelletier <virtualdogbert@gmail.com>
2022-10-30 14:23:48 -0400

    Broken up just for debugging.

--- grails-app/init/logback/test/g5/BootStrap.groovy
+++ grails-app/init/logback/test/g5/BootStrap.groovy
@@ -9,7 +9,7 @@ class BootStrap {
 
 // If I initialize with this code it works but with the configurator service it doesn't. Not sure what the difference is.
 //        LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory()
-//        LogbackDSLInitializer.init((LoggerContext) LoggerFactory.getILoggerFactory(), getClass().getClassLoader().getResourceAsStream("logback-config.groovy"))
+//        LogbackDSLInitializer.init(context, getClass().getClassLoader().getResourceAsStream("logback-config.groovy"))
 
 
         log.debug("***************************************** works?")
